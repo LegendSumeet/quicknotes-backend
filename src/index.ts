e5b65863458e90e75242db622cbe2a8c04addab5
legendsumeet <sumeetgithub@gmail.com>
2024-06-30 19:24:34 +0530

    added the jwt token

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,13 @@ import { Elysia } from "elysia";
 import { autoroutes } from "elysia-autoroutes";
 import { cors } from "@elysiajs/cors";
 import swagger from "@elysiajs/swagger";
-import pino from "pino";
+import pino, { P } from "pino";
 import staticPlugin from "@elysiajs/static";
-
-
+import jwt from "@elysiajs/jwt";
 const logger = pino({});
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 4000;
 const file = Bun.file("./public/index.html")
 const clients: WebSocket[] = [];
-
 const broadcast = (data: string) => {
     console.log(clients.length)
     console.log(data)
@@ -28,6 +26,13 @@ const QuickNotesApp = new Elysia()
     .use(
         autoroutes({
             routesDir: './routes',
+        }),
+    )
+    .use(
+        jwt({
+            name: "quicknoteapiv1token",
+            secret: process.env.JWT_SECRET!,
+            alg: "HS256",
         }),
     )
     .get("/", () => new Response(file, { status: 200 }))
@@ -72,7 +77,7 @@ const QuickNotesApp = new Elysia()
     .listen(port);
 
 
-    
+
 logger.info(
     `🦊 Quick Notes is running at https://apiv1.toystack.dev/ ${port}`,
 );
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -7,34 +7,42 @@ interface User {
 }
 
 export default (app: App) => {
-
-  app.post("/register", async ({ body, error }) => {
-    const { email, name } = body as User;
-    const existingUser = await findUserByEmail(email);
-    if (existingUser) {
-      return error(409.1, "User already exists");
-    }
-
-    const newUser = await createUser(email, name);
-    broadcast(JSON.stringify({ type: 'new_user', data: newUser}));
-
-    return new Response(
-      JSON.stringify({ status: "User created", data: newUser }),
-      {
-        status: 201,
+  app.post("/register", async ({ body, error, quicknoteapiv1token }) => {
+    try {
+      const { email, name } = body as User;
+      const existingUser = await findUserByEmail(email);
+      if (existingUser) {
+        return error(409.1, "User already exists");
       }
-    );
 
-  });
-
-  app.get("/login", async ({ query, error }) => {
-    const user = await findUserByEmail(query.email as string);
-    if (!user) {
-      return error(404.1, "User not found");
+      const newUser = await createUser(email, name);
+      broadcast(JSON.stringify({ type: 'new_user', data: newUser }));
+      const token = await quicknoteapiv1token.sign({ email: newUser.email });
+      return new Response(
+        JSON.stringify({ status: "User created", data: newUser, token }),
+        {
+          status: 201,
+        }
+      );
+    } catch (e) {
+      return error(500.1, JSON.stringify(e));
     }
-    return new Response(JSON.stringify(user), {
-      status: 200,
-    });
+  });
+  app.get("/login", async ({ query, error, quicknoteapiv1token, headers }) => {
+    try {
+      const user = await findUserByEmail(query.email as string);
+      const token = await quicknoteapiv1token.verify(headers.authorization);
+      if (!token) {
+        return error(401.1, "Invalid token");
+      } else {
+        return new Response(JSON.stringify(user), {
+          status: 200,
+        });
+      }
+
+    } catch (e) {
+      return error(500.1, JSON.stringify(e));
+    }
   });
 
   return app;
